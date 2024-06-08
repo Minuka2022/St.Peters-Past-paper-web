2222915fe6f13998e4eb989415089081fa9e0515
Minuka hansaja <108428943+Minuka2022@users.noreply.github.com>
2024-06-08 02:55:27 -0700

    navbar centerd

--- pages/Past-papers.php
+++ pages/Past-papers.php
@@ -454,10 +454,13 @@ if (isset($_SESSION['customerID'])) {
                 aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <img src="https://www.stpeterscollege.lk/wp-content/uploads/2020/09/SPC_Web-logo-New.png" alt="" style="margin-bottom:10px;">
+        <img src="https://www.stpeterscollege.lk/wp-content/uploads/2020/09/SPC_Web-logo-New.png" alt="" style="margin-bottom:10px; margin-right:-260px;">
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <div class="d-flex justify-content-center w-100">
                     <ul class="navbar-nav mb-2 mb-lg-0" style="font-weight:bold;">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#products-all">Events</a>
                         </li>
