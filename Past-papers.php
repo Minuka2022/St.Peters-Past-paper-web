dd911c975cdcabea700b807958d9156e701f6d91
ayodya <ayodyakcc@gmail.com>
2024-06-28 12:31:08 +0530

    Update Past-papers.php

--- Past-papers.php
+++ Past-papers.php
@@ -510,6 +510,79 @@ if (isset($_SESSION['customerID'])) {
         </center>
     </div>
       
+ <!-- fltetrt -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mt-5 shadow p-2 mb-5 bg-body rounded" id="products-all">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold">Grade 3 </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li>
+                    <form style="margin-right: 12px;">
+                        <select id="year_selector" name="year" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                            <option selected>Select Year</option>
+                            <option value="2024">2024</option>
+                            <option value="2023">2023</option>
+                            <option value="2022">2022</option>
+                            <option value="2021">2021</option>
+                        </select>
+                    </form>
+                </li>
+                <li>
+                    <form style="margin-right: 12px;">
+                        <select id="categories_list" name="filter_by" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                        </select>
+                    </form>
+                </li>
+                <li>
+                    <form style="margin-right: 12px;">
+                        <select id="term_selector" name="term" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                            <option selected>Select Term</option>
+                            <option value="1">Term 1</option>
+                            <option value="2">Term 2</option>
+                            <option value="3">Term 3</option>
+                        </select>
+                    </form>
+                </li>
+                <li>
+                    <form style="margin-right: 12px;">
+                        <select id="medium_selector" name="medium" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                            <option selected>Select Medium</option>
+                            <option value="english">English</option>
+                            <option value="french">French</option>
+                            <option value="spanish">Spanish</option>
+                        </select>
+                    </form>
+                </li>
+                <li>
+                    <form style="margin-right: 12px;">
+                        <select id="sort_selector" name="sort_by" class="form-select form-select-sm"
+                                aria-label=".form-select-sm example">
+                        </select>
+                    </form>
+                </li>
+                <li>
+                    <button type="button" class="btn btn-primary" id="filter-button">Filter</button>
+                </li>
+              
+            </ul>
+        </div>
+    </div>
+</nav>
+
+ <!-- fltetrt -->
+
+
+
+
 
 <div class="table-container">
     <table class="content-table">
